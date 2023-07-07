1829c4c5ab0fc41e9085db86f449e3c873c81ec4
juandaf <juandaforer@gmail.com>
2023-07-06 22:09:03 -0500

    Agragacion de metodos POST Para servicio y persona.+ Add De datos semilla para Persona y Servicio

--- ApiWebExamenEf/Contexto.cs
+++ ApiWebExamenEf/Contexto.cs
@@ -15,6 +15,12 @@ namespace ApiWebExamenEf
         {
             modelBuilder.Entity<Persona>(Persona =>
             {
+                List<Persona> personasInit = new List<Persona>();
+
+                personasInit.Add( new Persona() { PersonaId = "123456789", Nombres = "Juan David", Apellidos ="Forero", Email = "iotled@yopmil.com", Direccion = "Fúquene", NivelEstrato = Estrato.Medio});
+                personasInit.Add(new Persona() { PersonaId = "865292873", Nombres = "Marco", Apellidos = "Martonez", Email = "Martinez@yopmil.com", Direccion = "Fúquene", NivelEstrato = Estrato.Alto });
+                personasInit.Add(new Persona() { PersonaId = "642902773", Nombres = "Alonso", Apellidos = "Romero", Email = "Romero@yopmil.com", Direccion = "Fúquene", NivelEstrato = Estrato.Alto });
+
                 Persona.ToTable("Persona");
                 Persona.HasKey(p => p.PersonaId);
 
@@ -24,21 +30,30 @@ namespace ApiWebExamenEf
                 Persona.Property(p => p.Direccion);
                 Persona.Property(p => p.NivelEstrato);
 
+                Persona.HasData(personasInit);
+
             });
 
 
             modelBuilder.Entity<Servicio>(Servicio =>
             {
+                List<Servicio> serviciosInit = new List<Servicio>();
+                serviciosInit.Add(new Servicio() { ServicioId = Guid.NewGuid(), NommbreServicio = "Televison por cable",FechaCreacion = DateTime.Now, DescripcionServicio = "Sin descripcion"});
+                serviciosInit.Add(new Servicio() { ServicioId = Guid.NewGuid(), NommbreServicio = "Telelfonia Claro", FechaCreacion = DateTime.Now, DescripcionServicio = "Sin descripcion" });
+               
                 Servicio.ToTable("Servicio");
                 Servicio.HasKey(s => s.ServicioId);
 
                 Servicio.Property(s => s.NommbreServicio).IsRequired().HasMaxLength(100);
                 Servicio.Property(s => s.FechaCreacion).IsRequired();
                 Servicio.Property(s => s.DescripcionServicio);
+
+                Servicio.HasData(serviciosInit);
             });
 
             modelBuilder.Entity<DetallePaS>(DetallePaS =>
             {
+                
                 DetallePaS.ToTable("DetallePaS");
                 DetallePaS.HasKey(d => d.DetalleId);
                 DetallePaS.HasOne(d => d.Persona).WithMany(d => d.DetallePaSs).HasForeignKey(d => d.PersonaId);
--- ApiWebExamenEf/Program.cs
+++ ApiWebExamenEf/Program.cs
@@ -1,14 +1,41 @@
+using ApiWebExamenEf.Models;
 using ApiWebExamenEf;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSqlServer<Contexto>(builder.Configuration.GetConnectionString("ServicosDb"));
+builder.Services.AddSqlServer<Contexto>(builder.Configuration.GetConnectionString("ServiciosDb"));
 
 var app = builder.Build();
 
-app.MapGet("/DbConecction", async ([FromServices] Contexto contexto) => {
+
+app.MapGet("/Api/Personas", async ([FromServices] Contexto contexto) =>
+{
+    return Results.Ok(contexto.personas.Select(p => p));
+});
+
+app.MapPost("/Api/CargarPersona", async ([FromServices] Contexto contexto , [FromBodyAttribute] Persona persona) =>
+{
+    await contexto.AddAsync(persona);
+    await contexto.SaveChangesAsync();  
+    return Results.Ok();
+});
+
+app.MapPost("/Api/CargarServicio", async ([FromServices] Contexto contexto, [FromBodyAttribute] Servicio servicio) =>
+{
+    await contexto.AddAsync(servicio);
+    await contexto.SaveChangesAsync();
+    return Results.Ok();
+});
+
+app.MapGet("/", () => "Hello World!");
+
+
+
+app.MapGet("/DbConecction", async ([FromServices] Contexto contexto) =>
+{
     contexto.Database.EnsureCreated();
     return Results.Ok("Base de datos en memoria: " + contexto.Database.IsInMemory());
 });
