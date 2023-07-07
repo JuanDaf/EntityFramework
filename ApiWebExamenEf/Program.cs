b986635ff87a8049e45958f5aa2aa070796e4663
juandaf <juandaforer@gmail.com>
2023-07-06 22:40:32 -0500

    Agragacion de metodos PUT y DELETE para servicio y Persona

--- ApiWebExamenEf/Program.cs
+++ ApiWebExamenEf/Program.cs
@@ -15,6 +15,10 @@ app.MapGet("/Api/Personas", async ([FromServices] Contexto contexto) =>
 {
     return Results.Ok(contexto.personas.Select(p => p));
 });
+app.MapGet("/Api/Servicos", async ([FromServices] Contexto contexto) =>
+{
+    return Results.Ok(contexto.servicios.Select(p => p));
+});
 
 app.MapPost("/Api/CargarPersona", async ([FromServices] Contexto contexto , [FromBodyAttribute] Persona persona) =>
 {
@@ -28,6 +32,58 @@ app.MapPost("/Api/CargarServicio", async ([FromServices] Contexto contexto, [Fro
     await contexto.AddAsync(servicio);
     await contexto.SaveChangesAsync();
     return Results.Ok();
+});
+
+app.MapPost("/Api/CargarDetallesPaS", async ([FromServices] Contexto contexto, [FromBodyAttribute] DetallePaS detallePaS) =>
+{
+    await contexto.AddAsync(detallePaS);
+    await contexto.SaveChangesAsync();
+    return Results.Ok();
+});
+
+app.MapPut("/Api/ActulizarPersona/{id}", async ([FromServices] Contexto contexto, [FromBodyAttribute] Persona persona, [FromRoute] string id ) =>
+{
+    var personaActualizar = contexto.personas.Find(id);
+    if (personaActualizar != null)
+    {
+        personaActualizar.Nombres = persona.Nombres;
+        personaActualizar.Apellidos = persona.Apellidos;
+        personaActualizar.Email = persona.Email;
+        personaActualizar.Direccion = persona.Direccion;
+        personaActualizar.NivelEstrato = persona.NivelEstrato;
+
+        await contexto.SaveChangesAsync();
+        return Results.Ok();
+    }
+   
+    return Results.NotFound();
+});
+
+
+app.MapDelete("/Api/EliminarPersona/{id}", async ([FromServices] Contexto contexto, [FromRoute] string id) =>
+{
+    var personaActual = contexto.personas.Find(id);
+    if (personaActual != null)
+    {
+        contexto.Remove(personaActual);
+        await contexto.SaveChangesAsync();
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
+});
+
+app.MapDelete("/Api/EliminarServicio/{id}", async ([FromServices] Contexto contexto, [FromRoute] Guid id) =>
+{
+    var servicioActual = contexto.servicios.Find(id);
+    if (servicioActual != null)
+    {
+        contexto.Remove(servicioActual);
+        await contexto.SaveChangesAsync();
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
 });
 
 app.MapGet("/", () => "Hello World!");
